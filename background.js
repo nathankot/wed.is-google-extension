7a29958e87f5e11c4c278db801ff32b48e771741
Nathan Kot <me@nathankot.com>
2014-08-02 11:03:40 +1200

    Rudimentary working.

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
+var loaded = false;
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(tab.id, {file: "bookmarklet.js"})
+  if (!loaded) {
+    loaded = true;
+    chrome.tabs.executeScript(null, { file: "bookmarklet.js" });
+  } else {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { type: 'WEDIS_OPEN' });
+    });
+  }
 });
+

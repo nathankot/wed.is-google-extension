ea4a4735242a029a3c4828ed69d70c953ca6de73
Nathan Kot <me@nathankot.com>
2014-08-02 12:49:13 +1200

    Now detecting injection script correctly.

--- background.js
+++ background.js
@@ -1,13 +1,14 @@
 var loaded = false;
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  if (!loaded) {
-    loaded = true;
-    chrome.tabs.executeScript(null, { file: "bookmarklet.js" });
-  } else {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { type: 'WEDIS_OPEN' });
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { type: 'WEDIS_OPEN' }, function(response) {
+      if (!response) {
+        chrome.tabs.executeScript(null, { file: "bookmarklet.js" });
+      }
     });
-  }
+  });
+
 });
 
